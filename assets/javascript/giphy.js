5e0dc8e30a150a8ace5c4c2aca95207b4bba7323
William Budoyo <williambu07@gmail.com>
2018-06-20 10:00:26 -0400

    hw6

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -5,7 +5,7 @@
 //VARIABLES
 
 // string an array that hold all the names of anime
-var topic=["Naruto", "Bleach","One Piece", "Dragon Ball", "Death Note", "Attack of Titan"];
+var topic=["Naruto", "Bleach","One Piece Anime", "Dragon Ball", "Death Note", "Attack of Titan"];
 
 
 
@@ -46,7 +46,7 @@ function renderButtons(){
 
 // this function handles events when submit buttons is click, it store the value to our array
 function submitButtonToArray (){
-    
+   
 
     // everything it clicksubmit button, we add the text to the topic array
     $("#addAnime").on('click', function(){
@@ -68,13 +68,129 @@ function submitButtonToArray (){
      
     })// end click event 
 
-}// end inputString
+}// end submitButtonToArray
+
+
+
+// function  that call the giphy after click the button
+function buttonRenderGiphy(){
+
+    
+   
+    // event click listener  to all buttons
+    $(document).on("click",".anime", function(){
+    
+         $("#animes").empty();
+        // grabbing and storing to thisButton the value from the button
+        var thisButton = $(this).attr("data-name");
+
+         // Constructing a queryURL using the thisButton
+        var queryURL= "https://api.giphy.com/v1/gifs/search?q=";
+         var apiKey= "&api_key=eeDIH2DnsckEQM6XfYmbGhZxGy3gu09A";
+    
+         // combine all the url this button value and api key.
+         queryURL=queryURL+thisButton+apiKey;
+
+         // performing an AJAX request with the queryURl
+         $.ajax({
+             url:queryURL,
+             method: "GET"
+
+         })// end ajax
+
+         // After data comes back from the request
+        .then(function(response){
+
+            console.log(queryURL);
+            console.log(response);
+
+            var result = response.data;
+            //looping only 10 gif 
+            for( var i=1; i<=10; i++){
+               
+                // create and storing a div tag
+                var animeDiv=$("<div>");
+
+            //     // Creating a paragraph tag with the result item's rating
+               var p = $("<p>").text( i+" ) Rating: "  + result[i].rating);
+
+              // Creating and storing an image tag
+              var animeImage = $("<img>");
+
+              // Setting the src attribute of the image to a property pulled off the result item (must be still for default)
+              animeImage.attr("src",result[i].images.fixed_height_still.url);
+
+              //setting the attr data still from result item. this will be use to pause gif
+              animeImage.attr("data-still",result[i].images.fixed_height_still.url);
+
+            // set data state to still. this will be use to pause gif
+             animeImage.attr("data-state","still");
+
+             // set data-animate from result images. this will be use to animate the image
+             animeImage.attr("data-animate", result[i].images.fixed_height.url);
+
+             // set data-state to animate. this will be use to animate the image
+             animeImage.attr("data-state", "animate");
+
+             // add class to image
+             animeImage.addClass("gif");
+            // Appending the paragraph and image tag to the animalDiv
+            
+
+            animeDiv.append(p);
+            animeDiv.append(animeImage);
+
+            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+            $("#animes").append(animeDiv);
+
+            }// end for
+
+            stillOrAnimateImg();
+             
+        });// end (response)
+           
+        
+         
+
+    });//end button click listener
+}// end buttonRenderGiphy
+
+//function to turn still image to gif  or turn gif to still image
+function stillOrAnimateImg(){
+
+    //click event to image 
+    $(".gif").on("click", function(){
+        console.log("you click image");
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+
+        // if the image on still  state then we update the image src to data animate
+      if(state==="still"){
+        $(this).attr("src",$(this).attr("data-animate") );
+        // change the data-state to animate
+        $(this).attr("data-state","animate");
+
+      }
+      // else if the image on animate state then update the image src to data still
+      else{
+        $(this).attr("src",$(this).attr("data-still") );
+        // change the data-state to animate
+        $(this).attr("data-state","still");
+      }
+
+    })// end click event
+
+}// end stillOrAnimateImg
 
 //=========================================================================================================
 // MAIN PROCESS
 
 renderButtons();
 submitButtonToArray ();
+buttonRenderGiphy();
+
+
+
 
 
 //=========================================================================================================
